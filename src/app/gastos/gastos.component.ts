bdbb8b4037a39e7300c0727a1dc743a6c899cb4e
luizrosalba <luizrosalba@gmail.com>
2020-11-15 18:54:25 -0300

    adicionei o menu e o roteamento

---
+++ src/app/gastos/gastos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'luiz-gastos',
+  templateUrl: './gastos.component.html',
+  styleUrls: ['./gastos.component.css']
+})
+export class GastosComponent implements OnInit {
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
